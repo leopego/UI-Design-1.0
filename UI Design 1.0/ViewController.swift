8ccf23640f2b96179eff4e64148b97454b69d966
Leonardo Pego Martins <leonardopegomartins@gmail.com>
2020-07-13 11:41:09 -0300

    Initial commit

---
+++ UI Design 1.0/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UI Design 1.0
+//
+//  Created by mac on 30/06/20.
+//  Copyright © 2020 Mac Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
